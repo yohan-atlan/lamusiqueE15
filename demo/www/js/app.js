9afcca7fb3102e7a6e411686175841b640510933
Yohan Atlan <yohan.atlan@hetic.net>
2016-12-14 11:55:49 +0100

    add android platforms

--- demo/www/js/app.js
+++ demo/www/js/app.js
@@ -17,7 +17,6 @@ angular.module('starter', ['ionic','ngCordova'])
     }
   });
 })
-
 .config(function($stateProvider, $urlRouterProvider){
   $stateProvider.state('home',{
       url: '/home',
@@ -26,6 +25,10 @@ angular.module('starter', ['ionic','ngCordova'])
   $stateProvider.state('about',{
       url: '/about',
       templateUrl:'templates/about.html'
+  })
+  $stateProvider.state('sons',{
+      url: '/sons',
+      templateUrl:'templates/sons.html'
   })
 
   $urlRouterProvider.otherwise('/home')
@@ -116,4 +119,61 @@ angular.module('starter', ['ionic','ngCordova'])
     // });
   } ;
 
+})
+
+.controller('AudioController', function($scope,$ionicPlatform, $ionicActionSheet, $ionicLoading) {
+  var audio = [{
+      id: 1,
+      key: 'Son triste',
+      title: "Tristoune",
+      track: 'audio/The_Master.mp3',
+      genre: "This will be card Description"
+    }, {
+      id: 2,
+      key: 'Son joyeux',
+      title: "Joyeux",
+      track: 'audio/Give.mp3',
+      genre: "Alternative & Punk | Bright"
+    }, {
+      id: 3,
+      key: 'Son colère',
+      title: "Colère",
+      track: 'audio/colere.mp3',
+      genre: ""
+    }, ];
+
+    $scope.audioTracks = Array.prototype.slice.call(audio, 0);
+    // alert($scope.audioTracks);
+
+    $scope.player = {
+      key: '' // Holds a last active track
+    }
+
+    $ionicPlatform.ready(function() {
+
+      $scope.playTrack = function(track, key) {
+        // Preload an audio track before we play it
+        window.plugins.NativeAudio.preloadComplex(key, track, 1, 1, 0, function(msg) {
+          // If this is not a first playback stop and unload previous audio track
+          if ($scope.player.key.length > 0) {
+            window.plugins.NativeAudio.stop($scope.player.key); // Stop audio track
+            window.plugins.NativeAudio.unload($scope.player.key); // Unload audio track
+          }
+
+          window.plugins.NativeAudio.play(key); // Play audio track
+          $scope.player.key = key; // Set a current audio track so we can close it if needed
+        }, function(msg) {
+          alert('error: ' + msg); // Loading error
+        });
+      };
+
+      $scope.stopTrack = function() {
+          // If this is not a first playback stop and unload previous audio track
+          if ($scope.player.key.length > 0) {
+            window.plugins.NativeAudio.stop($scope.player.key); // Stop audio track
+            window.plugins.NativeAudio.unload($scope.player.key); // Unload audio track
+            $scope.player.key = ''; // Remove a current track on unload, it will break an app if we try to unload it again in playTrack function
+          }
+      };
+    });
 });
