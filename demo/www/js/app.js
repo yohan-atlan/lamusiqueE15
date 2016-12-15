cd49e7a2abdefcfd50fdf162e9353f1b48babbf3
Yohan Atlan <yohan.atlan@hetic.net>
2016-12-15 15:55:48 +0100

    fix app.Js

--- demo/www/js/app.js
+++ demo/www/js/app.js
@@ -110,9 +110,9 @@ angular.module('starter', ['ionic','ngCordova'])
     OCRAD(document.getElementById("pic"), function(value){
       self.hideLoading();
       text = value;
+      alert(text);
+      $state.go('player', {text: text});
     });
-    // alert(text);
-    $state.go('player', {text: text});
     // var AYLIENTextAPI = require('aylien_textapi');
     // var textapi = new AYLIENTextAPI({
     //   application_id: "be2e9ebf",
@@ -148,7 +148,7 @@ angular.module('starter', ['ionic','ngCordova'])
   var audio_progress_bar      = document.querySelector('.audio_progress_bar');
   var audio_info_title        = document.querySelector('.audio_info_title_h1');
   var audio_info_author       = document.querySelector('.audio_info_author_h1');
-  
+
   var songs = {"songs":[
     {"title":"Colère 1", "autor":"", "src":"audio/colere.mp3"},
     {"title":"Colère 2", "autor":"", "src":"audio/colere2.mp3"},
